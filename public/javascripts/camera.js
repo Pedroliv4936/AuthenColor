80e04ad7d79b172b6b165c0a390c27fb8612c0d6
Pedro Oliveira <pedrocaldasdeoliveira@gmail.com>
2021-05-06 13:18:47 +0100

    Desenho de interface na pagina

--- public/javascripts/camera.js
+++ public/javascripts/camera.js
@@ -23,7 +23,7 @@ function onLoad() {
       return new Promise(function (resolve, reject) {
         getUserMedia.call(navigator, constraints, resolve, reject);
       });
-    }
+    };
   }
 
   navigator.mediaDevices
@@ -42,6 +42,7 @@ function onLoad() {
     });
 
   detector = new AR.Detector();
+
   requestAnimationFrame(tick);
 }
 
@@ -54,7 +55,6 @@ function tick() {
     var markers = detector.detect(imageData);
     drawCorners(markers);
     drawId(markers);
-    //drawInterface();
     passwordPosition(markers);
   }
 }
@@ -109,47 +109,112 @@ function drawId(markers) {
       y = Math.min(y, corner.y);
     }
 
-    context.strokeText(markers[i].id, x, y)
+    context.strokeText(markers[i].id, x, y);
   }
 }
 
+var pos;
+
 function passwordPosition(markers) {
+  //alterar para controlar os dois arucos
   var corners, corner;
   var position = document.getElementById("position");
-
   for (i = 0; i !== markers.length; ++i) {
     corners = markers[i].corners;
     for (j = 0; j !== corners.length; j++) {
       corner = corners[j];
       if (corner.x < 200) {
         if (corner.y < 160) {
-          position.innerHTML = "Nordeste"
+          position.innerHTML = "Nordeste";
+          pos = "Nordeste";
         } else if (corner.y > 320) {
-          position.innerHTML = "Sudeste"
-        } else position.innerHTML = "Este"
-      } else if (corner.x > 200 && corner.x < 500) {
+          position.innerHTML = "Sudeste";
+          pos = "Sudeste";
+        } else {
+          position.innerHTML = "Este";
+          pos = "Este";
+        }
+      } else if (corner.x > 200 && corner.x < 400) {
         if (corner.y < 160) {
-          position.innerHTML = "Norte"
+          position.innerHTML = "Norte";
+          pos = "Norte";
         } else if (corner.y > 320) {
-          position.innerHTML = "Sul"
-        } else position.innerHTML = "Centro"
+          position.innerHTML = "Sul";
+          pos = "Sul";
+        } else {
+          position.innerHTML = "Centro";
+          pos = "Centro";
+        }
       } else if (corner.x > 400) {
         if (corner.y < 160) {
-          position.innerHTML = "Noroeste"
+          position.innerHTML = "Noroeste";
+          pos = "Noroeste";
         } else if (corner.y > 320) {
-          position.innerHTML = "Sudoeste"
+          position.innerHTML = "Sudoeste";
+          pos = "Sudoeste";
         }
-        else position.innerHTML = "Oeste"
+      } else {
+        position.innerHTML = "Oeste";
+        pos = "Oeste";
       }
-      console.log("corner x:", corner.x + ",corner y:", corner.y);
     }
+    console.log("corner x:", corner.x + " ,corner y:", corner.y);
   }
 }
 
-function test(){
-  window.location.replace('login.html');
+function setup() {
+  createCanvas(600, 500);
+  circle(300, 250, 50); //centro
+  circle(300, 50, 50); //norte
+  circle(100, 240, 50); //oeste
+  circle(170, 100, 50); //noroeste
+  circle(430, 100, 50); //sudoeste
+  circle(170, 400, 50); //nordeste
+  circle(430, 400, 50); //este
+  circle(500, 240, 50); //sudeste
+  circle(300, 450, 50); //sul
 }
 
-
+function draw() {
+  fill(255);
+  switch (pos) {
+    case "Centro":
+      circle(300, 250, 50);
+      console.log(pos);
+      break;
+    case "Norte":
+      circle(300, 50, 50);
+      console.log(pos);
+      break;
+    case "Oeste":
+      circle(100, 240, 50);
+      console.log(pos);
+      break;
+    case "Noroeste":
+      circle(170, 100, 50);
+      console.log(pos);
+      break;
+    case "Sudoeste":
+      circle(430, 100, 50);
+      console.log(pos);
+      break;
+    case "Nordeste":
+      circle(170, 400, 50);
+      console.log(pos);
+      break;
+    case "Este":
+      circle(430, 400, 50);
+      console.log(pos);
+      break;
+    case "Sudeste":
+      circle(500, 240, 50);
+      console.log(pos);
+      break;
+    case "Sul":
+      circle(300, 450, 50);
+      console.log(pos);
+      break;
+  }
+}
 
 window.onload = onLoad;
